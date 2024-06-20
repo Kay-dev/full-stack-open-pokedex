9941f413a432200ea9bf4d4e1d494b97f97e2e19
zhengwei <a932327192@gmail.com>
2024-06-19 21:50:32 -0400

    add e2e test case

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -5,3 +5,11 @@ describe('Pokedex', () => {
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
 })
+
+describe('Can open a particular Pokemon', () => {
+  it('front page can be opened', () => {
+    cy.visit('http://localhost:5000/')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+  })
+})
